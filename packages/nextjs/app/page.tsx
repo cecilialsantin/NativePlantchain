6d925969a3286747731f1633ca167fa1b8fe01d4
cecilialsantin <chikhakituti@gmail.com>
2025-02-05 14:57:47 -0300

    modificacion estilo en page

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -125,7 +125,7 @@ const Home = () => {
 
   if (!isConnected) {
     return (
-      <div className="flex items-center flex-col flex-grow pt-10">
+      <div className="flex items-center flex-col flex-grow pt-20">
         <h1 className="text-4xl font-bold mb-4">Native ~ PlanTokens</h1>
         <h3 className="text-1xl font-bold mb-4">by NativePlantchain ~ <span><FaLeaf/></span></h3>
         <p className="text-lg mb-4">Conecta tu wallet para continuar...</p>
